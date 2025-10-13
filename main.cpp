a2a5242753d962d2fe54e2490aaf2dcc9cd02e39
Cruplusplus <juancruzdominguez7@gmail.com>
2025-10-13 17:31:12 -0300

    main
    main con nueva clase, sencillito solo tendria que tener el menu y el game loop

--- main.cpp
+++ main.cpp
@@ -1,105 +1,25 @@
 #include <iostream>
+#include "Game_Class.h"
+#include "json.hpp"
 using namespace std;
+//using namespace sf;
 
-class Mazmorras{
-private:
-int idChunk;
-int chanceAparicion;
-int dificultad;
-public:
+int main() {
 
-};
+    //Inicializar el engine
+    Juego juego;
 
-class Personajes{
-private:
-int idPersonaje;
-int hp;
-int dmg;
-int lvl;
-int tipo; //(0 = jugador, 1 = enemigo, 2 = boss)
-public:
+    //Game loop
 
-};
+    while (juego.gameRunning() && !juego.getFinalizarJuego())
+    {
+        //update
+        juego.update();
+        //render
+        juego.render();
 
-class Enemigos : public Personajes{
-private:
-int idEnemigo;
-char nombre[30];
-int hpBase;
-int dmgBase;
-int lvlMin;
-int lvlMax;
-public:
-
-};
-
-class Mapa{
-private:
-int idMapa;
-int seed;
-int pisoActual;
-int totalPisos;
-int chunksTotales;
-int enemigosTotales;
-public:
-
-};
-
-class SavePartida{
-private:
-int idPartida;
-int idJugador;
-int idMapa;
-int pisoActual;
-int hpActual;
-bool inventarioGuardado;
-public:
-
-};
-
-class Inventario{
-private:
-int idJugador;
-int idObjeto;
-int cantidad;
-public:
-
-};
-
-class Objetos{
-private:
-int idObjeto;
-char nombre[30];
-int tipo;
-public:
-
-};
-
-class RecursosVisuales{ //{Sprites + TilesMapa +Animaciones}
-private:
-int idRecurso;
-char rutaArchivo[100];
-int ancho;
-int alto;
-char tipo;
-bool camina;
-public:
-
-};
-
-class VisualConfig{
-private:
-int resolucionX;
-int resolucionY;
-int volumenMusica;
-public:
-
-};
-
-int main()
-{
-
-
+        //drawgame
+    }
 
 return 0;
 }
